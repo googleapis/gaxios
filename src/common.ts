01e650260ed0c60eb709c72aec386c6cb88344c4
Stephen <stephenplusplus@users.noreply.github.com>
2019-02-06 14:16:13 -0500

    lint

--- src/common.ts
+++ src/common.ts
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {Agent} from 'https';
 import {AbortSignal} from 'abort-controller';
+import {Agent} from 'https';
 
 export class GaxiosError<T = any> extends Error {
   code?: string;
