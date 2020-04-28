4133312c11c95bd40c2643c59b1680802b71084e
Benjamin E. Coe <bencoe@google.com>
2020-04-28 10:11:36 -0700

    test: add test for multipart uploads (#286)

--- browser-test/test.browser.ts
+++ browser-test/test.browser.ts
@@ -14,7 +14,14 @@
 import assert from 'assert';
 import {describe, it} from 'mocha';
 import {request} from '../src/index';
+import {PassThrough} from 'stream';
+import * as stream from 'stream';
+import * as uuid from 'uuid';
 const port = 7172; // should match the port defined in `webserver.ts`
+
+function isReadableStream(obj: stream.Readable | string) {
+  return obj instanceof stream.Readable && typeof obj._read === 'function';
+}
 
 describe('💻 browser tests', () => {
   it('should just work from browser', async () => {
@@ -30,5 +37,44 @@ describe('💻 browser tests', () => {
     });
     assert.strictEqual(result.status, 200);
     assert.strictEqual(result.data, 'value');
+  });
+
+  it('should support multipart post from the browser', async () => {
+    const headers: {[key: string]: string} = {};
+    const multipart = [
+      {
+        'Content-Type': 'application/json',
+        body: JSON.stringify({hello: 'world'}),
+      },
+      {
+        'Content-Type': 'text/plain',
+        body: 'hello world!',
+      },
+    ];
+    const boundary = uuid.v4();
+    const finale = `--${boundary}--`;
+    headers['Content-Type'] = `multipart/related; boundary=${boundary}`;
+
+    let content = '';
+    for (const part of multipart) {
+      const preamble = `--${boundary}\r\nContent-Type: ${part['Content-Type']}\r\n\r\n`;
+      // rStream.push(preamble);
+      content += preamble;
+      if (typeof part.body === 'string') {
+        content += part.body;
+        content += '\r\n';
+      }
+    }
+    content += finale;
+    const result = await request({
+      headers,
+      data: content,
+      method: 'POST',
+      url: `http://localhost:${port}/path`,
+    });
+    assert.strictEqual(result.status, 200);
+    const parts = result.data as string[];
+    assert.strictEqual(parts[0], '{"hello":"world"}');
+    assert.strictEqual(parts[1], 'hello world!');
   });
 });
