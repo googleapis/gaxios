db4539c98666aece833cbb9bd52d53ded206978e
Sofia Leon <sofialeon@google.com>
2021-04-08 10:08:41 -0700

    build: fix browser tests

--- webpack-tests.config.js
+++ webpack-tests.config.js
@@ -22,13 +22,21 @@ module.exports = {
     alias: {
       '../../package.json': path.resolve(__dirname, 'package.json'),
     },
-  },
-  node: {
-    child_process: 'empty',
-    fs: 'empty',
-    crypto: 'empty',
-    net: 'empty',
-    tls: 'empty',
+    fallback: {
+      crypto: false,
+      child_process: false,
+      fs: false,
+      http2: false,
+      buffer: 'browserify',
+      process: false,
+      os: false,
+      querystring: false,
+      path: false,
+      stream: 'stream-browserify',
+      url: false,
+      util: false,
+      zlib: false,
+    },
   },
   module: {
     rules: [
