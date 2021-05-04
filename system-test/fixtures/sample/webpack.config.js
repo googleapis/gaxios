ad819c71d196ddc414ceede452202c25168f16d7
sofisl <55454395+sofisl@users.noreply.github.com>
2021-05-04 13:29:22 -0700

    build: fix browser tests (#382)

--- system-test/fixtures/sample/webpack.config.js
+++ system-test/fixtures/sample/webpack.config.js
@@ -23,17 +23,25 @@ module.exports = {
     alias: {
       '../../package.json': path.resolve(__dirname, 'package.json'),
     },
+    fallback: {
+      crypto: false,
+      child_process: false,
+      fs: false,
+      http2: false,
+      buffer: 'browserify',
+      process: false,
+      os: false,
+      querystring: false,
+      path: false,
+      stream: 'stream-browserify',
+      url: false,
+      util: false,
+      zlib: false,
+    },
   },
   output: {
     filename: 'bundle.min.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  node: {
-    child_process: 'empty',
-    fs: 'empty',
-    crypto: 'empty',
-    net: 'empty',
-    tls: 'empty',
   },
   module: {
     rules: [
