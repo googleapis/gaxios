1e385ca213bdeeeed41bb54db3861c0d81d9f8c0
Sofia Leon <sofialeon@google.com>
2021-05-24 16:34:12 -0400

    remove codecov

--- system-test/test.install.ts
+++ system-test/test.install.ts
@@ -23,7 +23,7 @@ import {promisify} from 'util';
 import {describe, it, before, after} from 'mocha';
 
 const keep = false;
-const mvp = (promisify(mv) as {}) as (...args: string[]) => Promise<void>;
+const mvp = promisify(mv) as {} as (...args: string[]) => Promise<void>;
 const ncpp = promisify(ncp);
 const stagingDir = tmp.dirSync({keep, unsafeCleanup: true});
 const stagingPath = stagingDir.name;
