a9ee23b5260d29736568ac02bc18f8a9afeb08e7
Ray Foss <ray@vblip.com>
2022-04-20 11:49:24 -0500

    Remove dependency on Node.js process
    https://github.com/googleapis/gaxios/issues/337
    
    This improves compatibility with blunders that don't implement hacks like Vite, as well as non Node.js JavaScript environments such as Browsers, Deno, v8js and WebContainers.

--- src/gaxios.ts
+++ src/gaxios.ts
@@ -63,10 +63,10 @@ let HttpsProxyAgent: any;
 
 function loadProxy() {
   const proxy =
-    process.env.HTTPS_PROXY ||
-    process.env.https_proxy ||
-    process.env.HTTP_PROXY ||
-    process.env.http_proxy;
+    process?.env?.HTTPS_PROXY ||
+    process?.env?.https_proxy ||
+    process?.env?.HTTP_PROXY ||
+    process?.env?.http_proxy;
   if (proxy) {
     HttpsProxyAgent = require('https-proxy-agent');
   }
