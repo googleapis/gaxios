809c6d17d8d1d3e6e9bd7c333e706b2d61508097
Daniel Bankhead <danielbankhead@google.com>
2024-01-31 10:17:47 -0800

    chore: remove note

--- src/common.ts
+++ src/common.ts
@@ -147,7 +147,7 @@ export interface GaxiosOptions {
     options: GaxiosOptions,
     defaultAdapter: (options: GaxiosOptions) => GaxiosPromise<T>
   ) => GaxiosPromise<T>;
-  url?: string; // TODO: | URL
+  url?: string;
   baseUrl?: string; // deprecated
   baseURL?: string;
   method?:
