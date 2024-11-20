246e845c3ac8edaa5f39d85c027d86105b6c91f8
Daniel Bankhead <danielbankhead@google.com>
2024-11-20 11:30:15 -0800

    chore: HOME/USER

--- .kokoro/samples-test.sh
+++ .kokoro/samples-test.sh
@@ -18,6 +18,9 @@ set -eo pipefail
 
 export NPM_CONFIG_PREFIX=${HOME}/.npm-global
 
+echo "HOME: $HOME"
+echo "USER: $USER"
+
 # Setup service account credentials.
 export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secret_manager/long-door-651-kokoro-system-test-service-account
 export GCLOUD_PROJECT=long-door-651
