622a62f21d765b126a71caff9e740e0c28e70ce0
Daniel Bankhead <danielbankhead@google.com>
2024-11-20 11:41:20 -0800

    chore: open up `NPM_CONFIG_PREFIX` perms

--- .kokoro/samples-test.sh
+++ .kokoro/samples-test.sh
@@ -16,7 +16,7 @@
 
 set -eo pipefail
 
-export NPM_CONFIG_PREFIX=${HOME}/.npm-global
+su - node -c "chmod 777 $NPM_CONFIG_PREFIX"
 
 # Setup service account credentials.
 export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/secret_manager/long-door-651-kokoro-system-test-service-account
